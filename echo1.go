24c535e37d7cab912fb45e8b0228b2a2ed9dc185
meihaibo <1253880904@qq.com>
2021-03-02 17:20:40 +0800

    0302

---
+++ echo1.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"strings"
+)
+func main()  {
+	var s,sep string
+	for i:=1;i<len(os.Args);i++ {
+		s +=sep+os.Args[i]
+		sep=" "
+	}
+	fmt.Println("1:"+s)
+	fmt.Println("2:"+strings.Join(os.Args[1:], " "))//类似php 的implode?
+
+	s,sep="",""
+	for _,value :=range os.Args[1:len(os.Args)] {//for配合range
+		s+=sep+value
+		sep=" "
+	}
+	fmt.Println("3:"+s)
+
+	fmt.Println(os.Args[1:])
+}
