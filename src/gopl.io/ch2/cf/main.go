d9ade7c5de7e787c7720f0aab5249c2b9996667a
meihaibo <1253880904@qq.com>
2021-03-03 14:47:03 +0800

    use gpols

--- src/gopl.io/ch2/cf/main.go
+++ src/gopl.io/ch2/cf/main.go
@@ -12,7 +12,7 @@ import (
 	"os"
 	"strconv"
 
-	"gopl.io/ch2/tempconv"
+	"ch2/tempconv"
 )
 
 func main() {
--- src/gopl.io/ch2/echo4/main.go
+++ src/gopl.io/ch2/echo4/main.go
@@ -17,6 +17,7 @@ var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "separator")
 
 func main() {
+	fmt.Println(*n)
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
